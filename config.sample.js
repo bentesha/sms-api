8e53b051fc8d951df3327046a8a610fbaefd73a8
tesha <benedict.tesha@jamaatech.com>
2018-09-22 22:52:37 +0300

    Initial commit

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+  smpp: {
+    host: 'http://hostname:port_number',
+    systemId: 'your_system_id',
+    password: 'your_password',
+    systemType: 'smpp'
+  }
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+const smpp = require("smpp");
+const config = require("./config");
+
+
+const session = smpp.connect(config.smpp.host, (error, status) => {
+  if(error){
+    consoloe.log("Connection error: ", error);
+  } else {
+    console.log("Connected", status);
+  }
+});
+
+const options = {
+  system_id: config.smpp.systemId,
+  password: config.smpp.password,
+  system_type: config.smpp.systemType
+};
+
+session.bind_transceiver(options, pdu => {
+  if(pdu.command_status == 0){
+    console.log('Connected');
+
+    //Sending message
+    const sms = {
+      destination_addr = '255713809050',
+      short_message = 'From Home Wifi'
+    };
+
+    session.sumbit_sm(sms, pdu => {
+      if(pdu.command_status == 0){
+        console.log('Message successfully sent');
+      } else {
+        console.log('Send message failed');
+        console.log('PDU status: ' + pdu.command_status);
+      }
+    })
+  } else {
+    console.log('Connection failed');
+  }
+})
